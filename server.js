4a9da3832cca912e0844ee33b31618ed8ebdb917
aysecolak474-coder <aysecolak474@gmail.com>
2026-09-01 22:26:20 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,11 +3,38 @@ const http = require("http");
 const PORT = process.env.PORT || 10000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/plain; charset=utf-8"
-    });
 
-    res.end("GAMEOS SERVER ONLINE");
+    if (req.url === "/") {
+        res.writeHead(200, {
+            "Content-Type": "text/plain; charset=utf-8"
+        });
+
+        res.end("GAMEOS SERVER ONLINE");
+        return;
+    }
+
+    if (req.url.startsWith("/game/")) {
+
+        const game = req.url.split("/game/")[1];
+
+        console.log("OYUN ISTENDI: " + game);
+
+        res.writeHead(200, {
+            "Content-Type": "application/json; charset=utf-8",
+            "Access-Control-Allow-Origin": "*"
+        });
+
+        res.end(JSON.stringify({
+            status: "ok",
+            game: game,
+            message: game + " baslatma istegi alindi"
+        }));
+
+        return;
+    }
+
+    res.writeHead(404);
+    res.end("404");
 });
 
 server.listen(PORT, "0.0.0.0", function () {
